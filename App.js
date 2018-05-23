50574ab846bbe2dddb6f4e5e94793c4b9aa6aa39
Victor Lee <myleevictor@gmail.com>
2018-05-23 14:19:08 +0800

    setup dependencies and DBURL connection in App

--- App.js
+++ App.js
@@ -1,5 +1,35 @@
-import React, { Component } from 'react';
-import express from "express";
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").load();
+}
+
+const express = require('express');
+const App = express()
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Key = require('./models/Key');
+const keys = require('./routes/keys');
+
+const isProduction = process.env.NODE_ENV === "production";
+
+const dbUrl = process.env.MONGODB_URI;
+mongoose.connect(dbUrl, {}).then(async () => {
+  console.log("Connected to mongo database at " + dbUrl);
+});
+
+mongoose.connect(dbUrl, async function (err) {
+  if (err)
+    throw err;
+  console.log("successfully connected to books_db");
+
+});
+
+if (!isProduction) {
+  mongoose.set("debug", true);
+}
+
+
+
 
 
 const PORT = process.env.PORT || 3003;
