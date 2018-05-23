a0a613572d92d6447cdbe9d03b2fd867f12ff2a5
Victor Lee <myleevictor@gmail.com>
2018-05-23 14:05:13 +0800

    reformat dir structure and remove boilder plate code in App.js

---
+++ App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import express from "express";
+
+
+const PORT = process.env.PORT || 3003;
+
+app.listen(PORT, () => {
+  console.log("Server is up and listening on:" + PORT);
+});
